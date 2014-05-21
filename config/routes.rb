7eeb66d21a18ed2fc663236a483405a5c0e3b237
Robert Honsby <rhonsby@gmail.com>
2014-05-21 10:59:04 -0700

    Add user controller and sign up view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Hackstarter::Application.routes.draw do
+  resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
