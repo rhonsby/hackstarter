7c124602a9daa27951bbdad702e1bdca4b207200
Robert Honsby <rhonsby@gmail.com>
2014-05-21 10:28:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackstarter::Application.config.secret_key_base = 'deab730d7ca9ddd6e3c467766d0a2c3b3e54093ca6a1286513c8c6043d6d5bfff3ae4166cdb6816fffa00620e487083ae657d9d13cb94198ac25221d6836d049'
