eddbda952d4446311a29af1c331e258787d09284
Robert Honsby <rhonsby@gmail.com>
2014-05-21 10:52:28 -0700

    Add a user model

---
+++ app/models/user.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)      not null
+#  password_digest :string(255)      not null
+#  session_token   :string(255)      not null
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false } 
+  validates :session_token, presence: true
+  validates :password_digest, presence: { message: "can't be blank" }
+  validates :password, length: { minimum: 6, allow_nil: true } 
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(params)
+    user = User.find_by_username(params[:username])
+    user.try(:is_password?, params[:password])
+  end
+
+  def password=(plain_text)
+    unless plain_text.empty?
+      @password = plain_text
+      self.password_digest = BCrypt::Password.create(plain_text)
+    end
+  end
+
+  def is_password?(plain_text)
+    BCrypt::Password.new(password_digest).is_password?(plain_text)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.session_token
+  end
+  
+  private
+
+  def self.generate_session_token
+    SecureRandom.hex
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+end
