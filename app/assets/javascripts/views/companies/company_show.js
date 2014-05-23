29f56b56227b5c099467119fd5c3fb0b06545d6a
Robert Honsby <rhonsby@gmail.com>
2014-05-22 22:57:46 -0700

    Change favicon, add tab navigation on show page

--- app/assets/javascripts/views/companies/company_show.js
+++ app/assets/javascripts/views/companies/company_show.js
@@ -6,12 +6,25 @@ Hackstarter.Views.CompanyShow = Backbone.View.extend({
     this.listenTo(this.model, 'sync', this.updateStats);
   },
 
+  events: {
+    "click a[data-toggle='pill']": 'handleTabSwitch',
+  },
+
+  handleTabSwitch: function (event) {
+    event.preventDefault();
+    var $tab = $(event.currentTarget);
+    $tab.tab('show');
+  },
+
   updateStats: function () {
     this.$('.investor-count').html(
       this.model.get('investors').length
     );
 
-    this.$('#company-show-photo').attr('src', this.model.escape('main_photo_url'));
+    // crappy fix, but works for now.
+    if (this.model.get('main_photo_url') !== 'missing_small.png') {
+      this.$('#company-show-photo').attr('src', this.model.escape('main_photo_url'));
+    }
   },
 
   render: function () {
