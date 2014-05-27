457aa295e6050eb79732d4c7efaf3fb7d3fe8118
Robert Honsby <rhonsby@gmail.com>
2014-05-27 14:42:42 -0700

    Add validation to user login form

--- app/assets/javascripts/views/users/login.js
+++ app/assets/javascripts/views/users/login.js
@@ -13,11 +13,24 @@ Hackstarter.Views.Login = Backbone.View.extend({
 
     session.save({}, {
       success: function (resp) {
+        view.hideError();
+
         var user = new Hackstarter.Models.User(resp.attributes, { parse: true });
         Hackstarter.loginUser(user);
         Backbone.history.navigate('', { trigger: true });
+      },
+      error: function (model, resp) {
+        view.showError();
       }
     });
+  },
+
+  showError: function () {
+    $('.auth-form h4').removeClass('hidden');
+  },
+
+  hideError: function () {
+    $('.auth-form h4').addClass('hidden');
   },
 
   render: function () {
